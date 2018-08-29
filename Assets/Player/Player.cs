32f560fa915ebe1ff0b747bfefe5b594e1bfac4f
LanceJZ <lance.zimmerman@gmail.com>
2018-08-28 23:10:41 -0700

    First Commit
    Copied project files for backup.

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] public float HealthAsPercentage = 100;
+
+    void Start ()
+	{
+
+	}
+
+	void Update ()
+	{
+
+	}
+}
---
+++ Assets/Utility.cs
@@ -0,0 +1,8 @@
+﻿public enum Layer
+{
+    Walkable = 8,
+    Enemy = 9,
+    Wall = 10,
+    Object = 11,
+    RaycastEndStop = -1
+}
