c1cbe2a6431af8893637bff478dd9afc75bbd0f8
LanceJZ <lance.zimmerman@gmail.com>
2018-09-10 16:10:42 -0700

    End of 67. Added more enemies.

--- Assets/Characters/Enemies/Enemy.cs
+++ Assets/Characters/Enemies/Enemy.cs
@@ -6,26 +6,28 @@ using UnityStandardAssets.Characters.ThirdPerson;
 public class Enemy : MonoBehaviour, IDamagable
 {
 	[SerializeField] int HealthMax = 10;
-    [SerializeField] int DamagePerAttack = 2;
+	[SerializeField] int DamagePerMeleeAttack = 2;
+    [SerializeField] int DamagePerRangeAttack = 4;
 	[SerializeField] float MeleeAttackRadius = 2;
-    [SerializeField] float RangeAttackRadius = 16;
-    [SerializeField] float DetectRadius = 5;
-    [SerializeField] float ChaseRadius = 20;
-    [SerializeField] bool HasRangedAttack;
-    [SerializeField] float FireCooldown = 1;
-    [SerializeField] GameObject Projectile;
-    [SerializeField] Vector3 AimOffset = new Vector3(0, 1, 0);
+	[SerializeField] float RangeAttackRadius = 16;
+	[SerializeField] float DetectRadius = 5;
+	[SerializeField] float ChaseRadius = 20;
+	[SerializeField] bool HasRangedAttack;
+	[SerializeField] float FireCooldown = 1;
+	[SerializeField] GameObject Projectile;
+	[SerializeField] Vector3 AimOffset = new Vector3(0, 1, 0);
 
 	public delegate void OnHealthChange(float healthChange);
 	public event OnHealthChange HealthChange;
 
-    Timer ShotTimer;
+	Timer ShotTimer;
 	//ThirdPersonCharacter TPCaracter;
 	AICharacterControl AIControl;
 	GameObject ThePlayer;
-    Transform SpawnPoint;
+	Player ThePlayerScript;
+	Transform SpawnPoint;
 	int Health;
-    bool PlayerSighted;
+	bool PlayerSighted;
 
 	public float HealthAsPercentage
 	{
@@ -37,109 +39,102 @@ public class Enemy : MonoBehaviour, IDamagable
 
 	void Start ()
 	{
-        Health = HealthMax;
+		Health = HealthMax;
 		ThePlayer = GameObject.FindGameObjectWithTag("Player");
+		ThePlayerScript = ThePlayer.GetComponent<Player>();
 		//TPCaracter = GetComponent<ThirdPersonCharacter>();
 		AIControl = GetComponent<AICharacterControl>();
-        SpawnPoint = transform.Find("ProjectileSpawnPoint");
+		SpawnPoint = transform.Find("ProjectileSpawnPoint");
 
-        ShotTimer = new Timer(FireCooldown);
-    }
+		ShotTimer = new Timer(FireCooldown);
+	}
 
-    void Update ()
+	void Update ()
 	{
 
-        float distToPlayer = Vector3.Distance(ThePlayer.transform.position, transform.position);
+		float distToPlayer = Vector3.Distance(ThePlayer.transform.position, transform.position);
 
-        if (PlayerSighted)
-        {
-            if (HasRangedAttack && ShotTimer.Expired)
-            {
-                if (distToPlayer >= RangeAttackRadius)
-                {
-                    print("Enemy moves to within Ranged Attack Radius.");
-                    AIControl.SetTarget(ThePlayer.transform);
-                }
-                else
-                {
-                    print("Enemy shots projectile!");
-                    GameObject shot = Instantiate(Projectile);
-                    shot.GetComponent<Projectile>().Fire(SpawnPoint.position,
-                        ThePlayer.transform.position + AimOffset, DamagePerAttack);
-                    AIControl.SetTarget(transform);
-                    ShotTimer.Reset();
-                }
-            }
-            else
-            {
-                if (distToPlayer <= ChaseRadius)
-                {
-                    AIControl.SetTarget(ThePlayer.transform);
-                }
+		if (PlayerSighted)
+		{
+			if (HasRangedAttack && ShotTimer.Expired)
+			{
+				if (distToPlayer >= RangeAttackRadius)
+				{
+					print("Enemy moves to within Ranged Attack Radius.");
+					AIControl.SetTarget(ThePlayer.transform);
+				}
+				else
+				{
+					print("Enemy shots projectile!");
+					GameObject shot = Instantiate(Projectile);
+					shot.GetComponent<Projectile>().Fire(SpawnPoint.position,
+						ThePlayer.transform.position + AimOffset, DamagePerRangeAttack);
+					AIControl.SetTarget(transform);
+					ShotTimer.Reset();
+				}
+			}
+			else
+			{
+				if (distToPlayer <= ChaseRadius)
+				{
+					AIControl.SetTarget(ThePlayer.transform);
+				}
 
-                if (distToPlayer <= DetectRadius && distToPlayer >= MeleeAttackRadius)
-                {
-                    print("Enemy spots player!");
-                    AIControl.SetTarget(ThePlayer.transform);
-                    PlayerSighted = true;
-                }
-            }
+				if (distToPlayer <= DetectRadius && distToPlayer >= MeleeAttackRadius)
+				{
+					print("Enemy spots player!");
+					AIControl.SetTarget(ThePlayer.transform);
+					PlayerSighted = true;
+				}
+			}
 
-            if (distToPlayer >= ChaseRadius)
-            {
-                print("Enemy lost sight of player.");
-                AIControl.SetTarget(transform);
-                PlayerSighted = false;
-            }
-        }
-        else
-        {
-            if (distToPlayer <= DetectRadius)
-            {
-                print("Enemy spots player!");
-                PlayerSighted = true;
-            }
-        }
+			if (distToPlayer >= ChaseRadius)
+			{
+				print("Enemy lost sight of player.");
+				AIControl.SetTarget(transform);
+				PlayerSighted = false;
+			}
+		}
+		else
+		{
+			if (distToPlayer <= DetectRadius)
+			{
+				print("Enemy spots player!");
+				PlayerSighted = true;
+			}
+		}
 
-        if (distToPlayer <= MeleeAttackRadius)
-        {
-            print("Enemy melee attacks Player!");
-            AIControl.SetTarget(transform);
-        }
-    }
+		if (distToPlayer <= MeleeAttackRadius)
+		{
+			print("Enemy melee attacks Player!");
+			AIControl.SetTarget(transform);
+            ThePlayerScript.TakeDamage(DamagePerMeleeAttack);
+		}
+	}
 
-    public void TakeDamage(int damage)
-    {
-        Health = (int)Mathf.Clamp((float)Health - damage, 0, HealthMax);
-        HealthChange(Health);
+	public void TakeDamage(int damage)
+	{
+		Health = (int)Mathf.Clamp((float)Health - damage, 0, HealthMax);
+		HealthChange(Health);
 
-        if (Health < 1)
-        {
-            Destroy(gameObject);
-        }
-    }
+		if (Health < 1)
+		{
+			Destroy(gameObject);
+		}
+	}
 
-    void OnDrawGizmos()
-    {
-        //Gizmos.color = Color.grey;
-        //Gizmos.DrawLine(transform.position, CurrentClickTarget);
-        //Gizmos.DrawSphere(CurrentClickTarget, 0.1f);
-        //Gizmos.DrawSphere(ClickPoint, 0.15f);
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.blue;
+		Gizmos.DrawWireSphere(transform.position, DetectRadius);
 
-        //Draw Melee attack sphere.
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(transform.position, DetectRadius);
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireSphere(transform.position, ChaseRadius);
 
-        //Draw Melee attack sphere.
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, ChaseRadius);
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, MeleeAttackRadius);
 
-        //Draw Melee attack sphere.
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, MeleeAttackRadius);
-
-        //Draw Range attack sphere.
-        Gizmos.color = Color.cyan;
-        Gizmos.DrawWireSphere(transform.position, RangeAttackRadius);
-    }
+		Gizmos.color = Color.cyan;
+		Gizmos.DrawWireSphere(transform.position, RangeAttackRadius);
+	}
 }
